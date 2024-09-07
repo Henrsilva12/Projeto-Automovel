3714826787ee2281a7a1b04169283e5d9284a68f
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-09-07 13:42:27 -0300

    Add initial project files and configurations

---
+++ Sistema de Reserva/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Login from './Pages/Login/Index'
+import Recuperar from '/Pages/Recuperar/indexRec'
+
+function App() {
+
+  return (
+    <Login />
+  )
+}
+
+export default App
